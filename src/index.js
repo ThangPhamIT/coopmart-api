9399b550449ca6bb9df5d1de67d0753db3fe2abb
ThangPham <pnthang@tma.com.vn>
2018-11-13 17:20:40 +0700

    initial source code api

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var dotenv = require('dotenv');
 var Promise = require('bluebird');
+var cors = require('cors');
 
 var authenticate = require('./routes/authenticate');
 var users = require('./routes/users');
@@ -11,6 +12,7 @@ var users = require('./routes/users');
 dotenv.config();
 
 const app = express();
+app.use(cors());
 var port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
@@ -20,18 +22,6 @@ mongoose.connect(process.env.MONGODB_URL, { useCreateIndex: true, useNewUrlParse
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
-// app.all('/*', function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin: *');
-//     res.setHeader('Access-Control-Allow-Credentials: true');
-//     res.setHeader('Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT');
-//     res.setHeader('Access-Control-Allow-Headers: accept, content-type, x-xsrf-token, x-csrf-token, authorization');
-//     if (req.method == 'OPTIONS') {
-//         res.status(200).end();
-//     } else {
-//         next();
-//     }
-// });
 
 app.get('/*', (req, res) => {
     res.render(path.join(__dirname, 'index'));
